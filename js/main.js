798fc13875b53061bb9a910fc293f4cfbac710a9
XY-ZHOU <731986251@qq.com>
2019-01-08 16:01:45 +0800

    get new todo

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ function addTodo() {
   }
   INPUTTXT.value = '';
 }
+
+function getNewTodo() {
+  if (INPUTTXT.value !== "") {
+    date = new Date().toLocaleString();
+    let content = {};
+    content.txt = INPUTTXT.value;
+    content.status = caseStatus;
+    let contentStr = objToString(content);
+    localStorage.setItem(date, contentStr);
+  }
+  if (btnStatus != 'compeleted') {
+    showList(date, INPUTTXT.value, caseStatus);
+  }
+  leftItems.innerHTML = 'Left Items:' + countLeft();
+}
