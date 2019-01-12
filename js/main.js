e7bb2b32ab872f4b2b5b7f31e47678b806832dbd
XY-ZHOU <731986251@qq.com>
2019-01-12 20:26:26 +0800

    modify function of adding todo

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@ const LISTS = document.getElementById('lists');
 const BTNS = document.getElementById('btns');
 let btnStatus;
 let storageItems = new StorageItems(localStorage);
+
+function addTodo(event) {
+  let input = document.getElementById('inputTxt');
+  if (event.keyCode == 13 && input.value) {
+    let newTodo = new Todo(input.value, false);
+    storageItems.addATodo(newTodo);
+    if (btnStatus != 'completed') {
+      showList(newTodo);
+    }
+  }
+  input.value = '';
+  showLeftAndClearCoCompleteted();
+}
