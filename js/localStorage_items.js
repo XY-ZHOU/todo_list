4edf87ea0489bc757bf394a624c82f8d2922015b
XY-ZHOU <731986251@qq.com>
2019-01-12 20:15:42 +0800

    function of adding todo

--- js/localStorage_items.js
+++ js/localStorage_items.js
@@ -2,4 +2,8 @@ class StorageItems {
   constructor(localStorage) {
     this.storageItems = localStorage;
   }
+  addATodo(newTodo) {
+    let date = new Date().getTime();
+    this.storageItems.setItem(date, JSON.stringify(newTodo));
+  }
 }
