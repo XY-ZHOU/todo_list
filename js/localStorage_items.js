221fb28af2230d797f090326041c285c598e598a
XY-ZHOU <731986251@qq.com>
2019-01-12 20:16:31 +0800

    function of finishing todo

--- js/localStorage_items.js
+++ js/localStorage_items.js
@@ -6,4 +6,14 @@ class StorageItems {
     let date = new Date().getTime();
     this.storageItems.setItem(date, JSON.stringify(newTodo));
   }
+  finishATodo(content) {
+    for (let i = 0; i < this.storageItems.length; i++) {
+      let key = this.storageItems.key(i);
+      let value = JSON.parse(this.storageItems[key]);
+      if (value.content.trim() === content) {
+        value.isCompleted = true;
+        this.storageItems.setItem(key, JSON.stringify(value));
+      }
+    }
+  }
 }
